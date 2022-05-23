677c829b27abd46e9e8168df002a0a4cc5fbf8ee
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-05-23 14:46:02 +0200

    create color generator

--- src/components/FilterColorsForm.tsx
+++ src/components/FilterColorsForm.tsx
@@ -40,10 +40,12 @@ const predefinedColors = [
 
 export default class FilterColors extends Component <MyProps> {
 
+
     render() {
         return (
             <>
                 <form>
+                    <p onClick={()=> console.log(this.props.colors)}>klik</p>
                     <label htmlFor="filterColors">Choose color:</label>
                     <select name="filterColors" id="filterColors">
                         <option value="1">Filter 1</option>
@@ -52,10 +54,22 @@ export default class FilterColors extends Component <MyProps> {
                         <option value="4">Filter 4</option>
                     </select>
                 </form>
-                {this.props.colors.map((color,idx)=>(
-                    <p key={idx}>{color.code}<span style={{backgroundColor: `${color.code}`}}>square</span></p>
-                    )
-                )}
+                <div className={"generator"}>
+                    {this.props.colors.map((color,idx)=>(
+                            //wiem że w poleceniu jest zabronione stylowanie inline, ale w tym przypadku
+                            // wydaje mi się jak sensowne rozwiązanie
+                            // i jest to jedyny wyjątek
+                            <div className={"generator__colorContainer"} key={idx}>
+                                <div className="generator__color">
+                                    <div className="generator__presentation" style={{backgroundColor: `${color.code}`}}/>
+                                    <p>{color.code}</p>
+                                </div>
+                                <button className="generator__button-remove" role={"button"}>remove from favorites</button>
+                            </div>
+                        )
+                    )}
+                </div>
+
             </>
         );
     }
